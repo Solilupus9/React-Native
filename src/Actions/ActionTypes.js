ad7f7cb9d722e465a4cbeaa55e2bae7df803fa50
Solilupus9 <95534734+Solilupus9@users.noreply.github.com>
2022-12-23 22:04:11 +0530

    Delete src directory

--- src/Actions/ActionTypes.js
+++
@@ -1,4 +0,0 @@
-export const SEARCH_BOX_UPDATED='search_box_updated';
-export const UPDATE_IMAGE_LIST='image_list_updated';
-export const TOGGLE_LOADER='loader_toggled';
-export const FILTER_IMAGE_LIST='image_list_filtered';
